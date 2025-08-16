fe9c470ae9bf0dea5bc8f9d9f4b566c64902c19a
Yanngb99 <112677322+Yanngb99@users.noreply.github.com>
2025-08-16 12:00:26 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -2,7 +2,7 @@
 module.exports = {
   SESSION_ID: "malvin~c69DwYSQ#Xuk7pPM0pRwuyJ9ttB37lmOuUwOezsCpSvN-EZuhez4",  // add your Session Id here
   
-  OWNER_NUMBER: "26371477857", // put your phone number here
+  OWNER_NUMBER: "2250596325518", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
